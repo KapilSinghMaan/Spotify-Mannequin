871391190fb77ed4134b163d9a6fe964ea4821c9
KapilSinghMaan <kapilmaan369@gmail.com>
2025-09-14 19:58:28 +0530

    Javascript

---
+++ js/script.js
@@ -0,0 +1,36 @@
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:5500/songs/")
+    let response = await a.text();
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    let songs = []
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split("/songs/")[1]);
+        }
+    }
+    return songs
+}
+
+async function main() {
+    let songs =await getSongs()
+    console.log(songs);
+
+    let songUL=document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML=songUL.innerHTML+`<li class="position-relative d-flex align-items-center border py-1 rounded-2 my-3 small">
+                            <i class="fa-solid fa-music mx-2"></i>
+                            <div class="songList-info">
+                                <div>${song.replaceAll("%20"," ")}</div>
+                                <div>Kapil Singh Maan</div>
+                            </div>
+                            <div class="songList-play position-absolute end-0 me-1">
+                                <i class="fa-solid fa-play"></i>
+                            </div>
+                        </li>`;
+    }
+}
+
+main()
